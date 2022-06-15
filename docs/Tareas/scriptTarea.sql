8695107337ca78ddbca8dbc28153ef920cab88fb
LeidyRomero <40676761+LeidyRomero@users.noreply.github.com>
2022-06-15 20:26:59 +0000

    Add files via upload

---
+++ docs/Tareas/scriptTarea.sql
@@ -0,0 +1,42 @@
+USE Estudiante_43;
+
+USE Estudiante_43;
+--------------------- Modelo de hecho movimiento
+CREATE TABLE Fecha (
+Fecha DATETIME, 
+Dia TINYINT, 
+Mes VARCHAR(20), 
+Anio SMALLINT,
+Numero_semana_ISO TINYINT,
+PRIMARY KEY(Fecha));
+
+CREATE TABLE Producto (
+ID_Producto SMALLINT, 
+Nombre VARCHAR(100),
+Marca VARCHAR(30), 
+Paquete_de_compra VARCHAR(20), 
+Color VARCHAR(10), 
+Necesita_refrigeracion BOOLEAN, 
+Dias_tiempo_entrega SMALLINT, 
+cantidad_por_salida INT, 
+Precio_minorista_recomendado FLOAT, 
+Paquete_de_venta VARCHAR(20), 
+Precio_unitario FLOAT,
+PRIMARY KEY(ID_Producto));
+
+CREATE Proveedor(
+ID_proveedor INT
+Nombre VARCHAR(20),
+Categoria VARCHAR(20),
+Contacto_principal VARCHAR(30),
+Referencia VARCHAR(30),
+Dias_pago INT, 
+Codigo_postal INT,
+PRIMARY_KEY(ID_proveedor)
+);
+
+CREATE TipoTransaccion(
+ID_Tipo_transaccion TINYINT,
+Tipo VARCHAR(20),
+PRIMARY_KEY(ID_Tipo_transaccion)
+);
